267c4d0ee5a481b96af53d38e45102237ddd5872
Ruben <rubenfergon2001@gmail.com>
2023-06-19 15:49:55 +0200

    Modificación pruebapy.py

--- pruebapy.py
+++ pruebapy.py
@@ -1,83 +1,59 @@
-const float theta_spacing = 0.07;
-const float phi_spacing   = 0.02;
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
-const float R1 = 1;
-const float R2 = 2;
-const float K2 = 5;
-// Calculate K1 based on screen size: the maximum x-distance occurs
-// roughly at the edge of the torus, which is at x=R1+R2, z=0.  we
-// want that to be displaced 3/8ths of the width of the screen, which
-// is 3/4th of the way from the center to the side of the screen.
-// screen_width*3/8 = K1*(R1+R2)/(K2+0)
-// screen_width*K2*3/(8*(R1+R2)) = K1
-const float K1 = screen_width*K2*3/(8*(R1+R2));
+theta_spacing = 0.07
+phi_spacing = 0.02
 
-render_frame(float A, float B) {
-  // precompute sines and cosines of A and B
-  float cosA = cos(A), sinA = sin(A);
-  float cosB = cos(B), sinB = sin(B);
+R1 = 1
+R2 = 2
+K2 = 5
 
-  char output[0..screen_width, 0..screen_height] = ' ';
-  float zbuffer[0..screen_width, 0..screen_height] = 0;
+screen_width = 80
+screen_height = 24
 
-  // theta goes around the cross-sectional circle of a torus
-  for (float theta=0; theta < 2*pi; theta += theta_spacing) {
-    // precompute sines and cosines of theta
-    float costheta = cos(theta), sintheta = sin(theta);
+# Calculate K1 based on screen size
+K1 = screen_width * K2 * 3 / (8 * (R1 + R2))
 
-    // phi goes around the center of revolution of a torus
-    for(float phi=0; phi < 2*pi; phi += phi_spacing) {
-      // precompute sines and cosines of phi
-      float cosphi = cos(phi), sinphi = sin(phi);
-    
-      // the x,y coordinate of the circle, before revolving (factored
-      // out of the above equations)
-      float circlex = R2 + R1*costheta;
-      float circley = R1*sintheta;
+def render_frame(A, B):
+    cosA = math.cos(A)
+    sinA = math.sin(A)
+    cosB = math.cos(B)
+    sinB = math.sin(B)
 
-      // final 3D (x,y,z) coordinate after rotations, directly from
-      // our math above
-      float x = circlex*(cosB*cosphi + sinA*sinB*sinphi)
-        - circley*cosA*sinB; 
-      float y = circlex*(sinB*cosphi - sinA*cosB*sinphi)
-        + circley*cosA*cosB;
-      float z = K2 + cosA*circlex*sinphi + circley*sinA;
-      float ooz = 1/z;  // "one over z"
-      
-      // x and y projection.  note that y is negated here, because y
-      // goes up in 3D space but down on 2D displays.
-      int xp = (int) (screen_width/2 + K1*ooz*x);
-      int yp = (int) (screen_height/2 - K1*ooz*y);
-      
-      // calculate luminance.  ugly, but correct.
-      float L = cosphi*costheta*sinB - cosA*costheta*sinphi -
-        sinA*sintheta + cosB*(cosA*sintheta - costheta*sinA*sinphi);
-      // L ranges from -sqrt(2) to +sqrt(2).  If it's < 0, the surface
-      // is pointing away from us, so we won't bother trying to plot it.
-      if (L > 0) {
-        // test against the z-buffer.  larger 1/z means the pixel is
-        // closer to the viewer than what's already plotted.
-        if(ooz > zbuffer[xp,yp]) {
-          zbuffer[xp, yp] = ooz;
-          int luminance_index = L*8;
-          // luminance_index is now in the range 0..11 (8*sqrt(2) = 11.3)
-          // now we lookup the character corresponding to the
-          // luminance and plot it in our output:
-          output[xp, yp] = ".,-~:;=!*#$@"[luminance_index];
-        }
-      }
-    }
-  }
+    output = [[' ' for _ in range(screen_width)] for _ in range(screen_height)]
+    zbuffer = [[0 for _ in range(screen_width)] for _ in range(screen_height)]
 
-  // now, dump output[] to the screen.
-  // bring cursor to "home" location, in just about any currently-used
-  // terminal emulation mode
-  printf("\x1b[H");
-  for (int j = 0; j < screen_height; j++) {
-    for (int i = 0; i < screen_width; i++) {
-      putchar(output[i,j]);
-    }
-    putchar('\n');
-  }
-  
-}
+    for theta in np.arange(0, 2 * np.pi, theta_spacing):
+        costheta = np.cos(theta)
+        sintheta = np.sin(theta)
+
+        for phi in np.arange(0, 2 * np.pi, phi_spacing):
+            cosphi = np.cos(phi)
+            sinphi = np.sin(phi)
+
+            circlex = R2 + R1 * costheta
+            circley = R1 * sintheta
+
+            x = circlex * (cosB * cosphi + sinA * sinB * sinphi) - circley * cosA * sinB
+            y = circlex * (sinB * cosphi - sinA * cosB * sinphi) + circley * cosA * cosB
+            z = K2 + cosA * circlex * sinphi + circley * sinA
+            ooz = 1 / z
+
+            xp = int(screen_width / 2 + K1 * ooz * x)
+            yp = int(screen_height / 2 - K1 * ooz * y)
+
+            L = cosphi * costheta * sinB - cosA * costheta * sinphi - sinA * sintheta + cosB * (cosA * sintheta - costheta * sinA * sinphi)
+
+            if L > 0:
+                if ooz > zbuffer[xp][yp]:
+                    zbuffer[xp][yp] = ooz
+                    luminance_index = int(L * 8)
+                    output[xp][yp] = ".,-~:;=!*#$@"[luminance_index]
+
+    plt.imshow(output, cmap='gray', origin='lower')
+    plt.axis('off')
+    plt.show()
+
+# Ejemplo de uso:
+render_frame(0, 0)
